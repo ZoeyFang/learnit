3ff575fa0d367f8adac4066437404ef30c29cf73
zoey.fang <zoey.fang@outlook.com>
2017-12-05 21:33:46 +0800

    update shell skills

---
+++ hello.py
@@ -0,0 +1,10 @@
+#name = input()
+#print("hello",name)
+#print("hello world!");
+#print("hello","world")
+
+name = input()
+if name == 'fang':
+    print('I\'m fang')
+else:
+    print('I\'m not fang')
---
+++ push.sh
@@ -0,0 +1,4 @@
+git add shell.txt
+git commit -m "update shell skills"
+git push github master
+git push gitee master
